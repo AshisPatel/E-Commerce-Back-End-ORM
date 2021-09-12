26dbebccef5396723c441a7147d76bc1363969ef
AP <ashis.n.patel@gmail.com>
2021-09-12 17:19:53 -0500

    Add ProductTag and Tag model & associations

--- models/index.js
+++ models/index.js
@@ -15,8 +15,17 @@ Category.hasMany(Product, {
 });
 
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  as: 'tags',
+  foriegnKey: 'product_id'
+});
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  as: 'tags',
+  foreignKey: 'tag_id'
+});
 
 module.exports = {
   Product,
